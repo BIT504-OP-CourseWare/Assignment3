fe3e279dd6e64932d1e94180dfa63dc96aff3512
louzadm <louzadm@WS4302.TEL>
2020-10-12 16:22:17 +1300

    Todo Brick done

--- Assignment3/src/Brick.java
+++ Assignment3/src/Brick.java
@@ -8,15 +8,16 @@ public class Brick extends Sprite {
 		// TODO: Set x using the parameter
 		setX(x);
 		// TODO: Set y using the parameter. See above for X
-		
+		setY(y);
 		
 		// TODO: Set the width and height of the brick using Settings.BRICK_WIDTH/HEIGHT
-		setWidth(???); //use a parameter from the settings to assign a value to ???
-		               //Do the same for height using appropriate method and parameter setting
+		setWidth(Settings.BRICK_WIDTH); //use a parameter from the settings to assign a value to ???
+		setHeight(Settings.BRICK_HEIGHT); //Do the same for height using appropriate method and parameter setting
 	}
 
 	public boolean isBroken() {
-		return false;	// TODO: Return the correct variable. //You can't write return false;
+		
+		return broken;	// TODO: Return the correct variable. //You can't write return false;
 		                // or return trues; it has to be dynamic. Which variable contains the status
 		                // of a Bricks object!?
 	}
@@ -24,7 +25,7 @@ public class Brick extends Sprite {
 		// TODO: Set the broken variable using the parameter given
 		//again if you know which varaible contains the status of the Brick object from the 
 		//above task then you could set its value as,
-		??? = b;
+		broken = b;
 	}
 	
 	public void paint(Graphics g) {
